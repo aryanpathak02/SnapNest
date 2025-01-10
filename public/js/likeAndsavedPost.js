00d26a144c6759b8fe108cc9e048ab7a98875fc8
aryanpathak02 <133569150+aryanpathak02@users.noreply.github.com>
2025-01-11 00:43:13 +0530

    Update likeAndsavedPost.js
    change the api link to the serve

--- public/js/likeAndsavedPost.js
+++ public/js/likeAndsavedPost.js
@@ -9,7 +9,7 @@ function savePost(postId, btn) {
     const originalColor = btn.getAttribute('fill');
     btn.setAttribute('fill', '#000000'); 
 
-    sendPostRequest('http://localhost:8080/savedPost', postId)
+    sendPostRequest('http://snapnest-u9zi.onrender.com/savedPost', postId)
 
         .then(response => {
             console.log('Post save response:', response.data);
@@ -56,7 +56,7 @@ function likePost(postId, btn) {
     pathElement.setAttribute('fill', '#e00b0b');
     pathElement.setAttribute('stroke', 'none');
 
-    sendPostRequest('http://localhost:8080/like', postId)
+    sendPostRequest('http://snapnest-u9zi.onrender.com/like', postId)
 
         .then(response => {
             console.log('Post like response:', response.data);
